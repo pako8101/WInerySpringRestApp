c5797a32e7addb90abb6860c81dfec217a3b5f9e
pako8101 <pako810129@gmail.com>
2024-08-17 16:18:08 +0300

    LoginController login.html form some css

---
+++ src/main/java/com/kamenov/wineryspringrestapp/web/LoginController.java
@@ -0,0 +1,51 @@
+package com.kamenov.wineryspringrestapp.web;
+
+
+import com.kamenov.wineryspringrestapp.models.dto.UserLoginDto;
+import com.kamenov.wineryspringrestapp.service.UserService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/users")
+public class LoginController {
+
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @ModelAttribute
+    public UserLoginDto userLoginBindingModel() {
+        return new UserLoginDto();
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        if (!model.containsAttribute("isFound")) {
+            model.addAttribute("isFound", true);
+        }
+
+        return "login";
+    }
+
+
+    @PostMapping("/login-error")
+    public String onFailedLogin(@ModelAttribute(UsernamePasswordAuthenticationFilter
+            .SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+                                RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username);
+        redirectAttributes.addFlashAttribute("bad_credentials", true);
+
+//        redirectAttributes.addFlashAttribute("username", username);
+        return "redirect:/users/login";
+
+    }
+}
