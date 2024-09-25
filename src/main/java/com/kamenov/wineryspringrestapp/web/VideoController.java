fa33b6b0e4fe975529b2d36a7fcfc14bde8a5bc8
pako8101 <pako810129@gmail.com>
2024-09-25 10:50:41 +0300

    cart view

--- src/main/java/com/kamenov/wineryspringrestapp/web/VideoController.java
+++ src/main/java/com/kamenov/wineryspringrestapp/web/VideoController.java
@@ -10,7 +10,7 @@ public class VideoController {
 
     @GetMapping("/wine-video")
     public String wineVideo(@RequestParam(name = "id",
-    required = false,defaultValue = "v=lRxOrS8b8BQ")String videoId, Model model) {
+    required = false,defaultValue = "lRxOrS8b8BQ")String videoId, Model model) {
        model.addAttribute("videoId", videoId);
         return "wine-video";
     }
