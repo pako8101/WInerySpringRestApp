bfa8948bb53923abf47b8e9d933477fdd8b8a68a
pako8101 <pako810129@gmail.com>
2024-09-25 10:15:19 +0300

    video controller and visualisation impl

---
+++ src/main/java/com/kamenov/wineryspringrestapp/web/VideoController.java
@@ -0,0 +1,18 @@
+package com.kamenov.wineryspringrestapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class VideoController {
+
+    @GetMapping("/wine-video")
+    public String wineVideo(@RequestParam(name = "id",
+    required = false,defaultValue = "v=lRxOrS8b8BQ")String videoId, Model model) {
+       model.addAttribute("videoId", videoId);
+        return "wine-video";
+    }
+
+}
