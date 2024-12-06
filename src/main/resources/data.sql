0745a707f0400be8a1d7f058b63c27b74762ba30
pako8101 <pako810129@gmail.com>
2024-12-06 23:26:22 +0200

    UUID sequence

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,20 @@
-INSERT INTO roles VALUES (1, 'USER'), (2, 'ADMIN');
+# INSERT INTO roles VALUES (1, 'USER'), (2, 'ADMIN');
+#
+# INSERT INTO `user` (`id`, `email`, `full_name`, `password`, `uuid`)
+# VALUES
+#     (1,'lachezar@example.com','Lachezar','Balev','8dded4dc9b97fe3124a0b5397e3ab5251046e6ffaf0fcf6b32c6eb9fea7b1337bfa48a886c8c064adebeb5ff05b84300','1cc6e990-3699-4633-8c68-f148bec2748a');
+#
+# INSERT INTO `user_roles` (`user_entity_id`, `roles_id`)
+# VALUES
+#     (1, 1),
+#     (1, 2);
 
-INSERT INTO `user` (`id`, `email`, `full_name`, `password`, `uuid`)
-VALUES
-    (1,'lachezar@example.com','Lachezar','Balev','8dded4dc9b97fe3124a0b5397e3ab5251046e6ffaf0fcf6b32c6eb9fea7b1337bfa48a886c8c064adebeb5ff05b84300','1cc6e990-3699-4633-8c68-f148bec2748a');
+# INSERT INTO roles VALUES (1, 'USER'), (2, 'ADMIN');
 
-INSERT INTO `user_roles` (`user_entity_id`, `roles_id`)
-VALUES
-    (1, 1),
-    (1, 2);
+INSERT INTO `user` (`id`,`registration_email_send`,`age`,`email`,`full_name`,`password`,`username`,`image_id`,`uuid`)
+VALUES (17,1,33,'alexcheto2013@abvw.bg','kamika','$2a$10$6mr.kBJXB5lFrA1/jeLeWe5ozLjIiBlfnfS/fwyeFOceMMq8t3yrW','alexandur',2,'baf8faaa-eede-451d-83a4-9c3e9b35f660');
+
+# INSERT INTO `user_roles` (`user_entity_id`, `roles_id`)
+# VALUES
+#     (1, 1),
+#     (1, 2);
